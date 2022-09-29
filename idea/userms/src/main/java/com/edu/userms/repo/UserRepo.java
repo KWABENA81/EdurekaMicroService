10b70bdd44f1c89d1d4d4577306e89cdf8e078f3
Solomon A Sefa <Solomon.Sefa@gmail.com>
2022-09-29 19:21:35 -0400

    initially on window but moving to ubuntu

---
+++ idea/userms/src/main/java/com/edu/userms/repo/UserRepo.java
@@ -0,0 +1,19 @@
+package com.edu.userms.repo;
+
+
+
+import com.edu.userms.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+ public interface UserRepo extends JpaRepository<User, Integer> {
+    List<User> findAll();
+
+    Optional<User> findById(Integer id);
+
+    User save(User user);
+}
---
+++ idea/userms/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into users_data (name) values ('John Doe');
+insert into users_data (name) values ('Jane Doe');
+insert into users_data (name) values ('Rocky Balboa');
+insert into users_data (name) values ('Elizabeth');
